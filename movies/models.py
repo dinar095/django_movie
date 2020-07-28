15d4ee93da79552b2f091294558378bdd4accfb4
Dinar <dinar095@hotmail.com>
2020-07-28 13:57:29 +0300

    Add model Category

--- movies/models.py
+++ movies/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     "Категории"
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
